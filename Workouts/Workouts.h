8249a2dfe907cd5411f0824e60633672335e304a
Aaron Connolly <aaron.connolly@live.com>
2018-03-19 10:56:19 -0400

    Initial Commit

---
+++ Workouts/Workouts.h
@@ -0,0 +1,19 @@
+//
+//  Workouts.h
+//  Workouts
+//
+//  Created by Aaron Connolly on 3/19/18.
+//  Copyright © 2018 Top Turn Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Workouts.
+FOUNDATION_EXPORT double WorkoutsVersionNumber;
+
+//! Project version string for Workouts.
+FOUNDATION_EXPORT const unsigned char WorkoutsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Workouts/PublicHeader.h>
+
+
